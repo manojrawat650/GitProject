4c52ebd853f05ccfd3ba2445d8659e17df77408c
Manoj <manojrawat650@gmail.com>
2017-01-06 02:42:00 +0530

    commit it

--- src/ABC.java
+++ src/ABC.java
@@ -4,6 +4,8 @@ public class ABC {
 	public static void main(String[] args) {
 		System.out.println("this is class ABC");
 		System.out.println("adding a simple change");
+		System.out.println("checked out a local branch");
+		System.out.println("chekced out local again");
 		
 
 	}
