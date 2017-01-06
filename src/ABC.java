e89decc67fe784d68b2a7926d6b97c6718fac7f1
Manoj <manojrawat650@gmail.com>
2017-01-06 12:56:05 +0530

    committing to local

--- src/ABC.java
+++ src/ABC.java
@@ -5,7 +5,10 @@ public class ABC {
 		System.out.println("this is class ABC");
 		System.out.println("adding a simple change");
 		System.out.println("checked out a local branch");
+		System.out.println("adding a statement through local ");
 		System.out.println("chekced out local again");
+		System.out.println("making changes in original eclipse");
+		
 		
 
 	}
