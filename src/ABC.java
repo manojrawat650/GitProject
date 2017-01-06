e0b7a7487073ab568feca48c2feb090aacaa82f7
Manoj <manojrawat650@gmail.com>
2017-01-06 12:48:52 +0530

    commit local

--- src/ABC.java
+++ src/ABC.java
@@ -4,7 +4,7 @@ public class ABC {
 	public static void main(String[] args) {
 		System.out.println("this is class ABC");
 		System.out.println("adding a simple change");
-		System.out.println("checked out a local branch")
+		System.out.println("checked out a local branch");
 		System.out.println("chekced out local again");
 		System.out.println("adding through local");
 		System.out.println("adding another through local");
